068d37a9ff368f8af18d56c36045a1cf45d62c28
Kay <kaybode@gmx.de>
2018-04-17 15:03:36 +0200

    Log anfang

--- app/src/main/java/com/example/mmw/testapplication/MainActivity.java
+++ app/src/main/java/com/example/mmw/testapplication/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final String LOG_TAG = "LOGTAG";
+    public void log(String line){
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
