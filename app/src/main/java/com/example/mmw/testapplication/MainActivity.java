ed898456608adab0bbe95f21a727018f86c514f5
Kay <kaybode@gmx.de>
2018-04-24 00:12:02 +0200

    Easy Ident Call aber alles nur gebastelt

--- app/src/main/java/com/example/mmw/testapplication/MainActivity.java
+++ app/src/main/java/com/example/mmw/testapplication/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.mmw.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    public final String LOG_TAG = "LOGTAG";
-    public void log(String line){
+    //public final String LOG_TAG = "LOGTAG";
 
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void OnClick(View view){
+        Intent i = new Intent(this,AddDiscussionParticipant.class);
+        startActivity(i);
     }
 
 
